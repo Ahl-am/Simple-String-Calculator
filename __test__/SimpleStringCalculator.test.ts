3cddcdd73f8909b0c6038c87bf65813e24da2dbc
Ahl-am <ahlam.moh.ibrahim@gmail.com>
2024-05-19 00:13:53 +0300

    ADD: Third Case (Two Numbers)

--- __test__/SimpleStringCalculator.test.ts
+++ __test__/SimpleStringCalculator.test.ts
@@ -7,4 +7,7 @@ describe("Test SimpleStringCalculator", () => {
   it("Should add one number in the string", () => {
     expect(sumFunction("1")).toEqual(1);
   });
+  it("Should add two number in the string", () => {
+    expect(sumFunction("1,2")).toEqual(3);
+  });
 });
--- src/SimpleStringCalculator.ts
+++ src/SimpleStringCalculator.ts
@@ -1,3 +1,11 @@
 export const sumFunction = (stringToAdd: string): number => {
-  return Number(stringToAdd);
-};
+  // if "1,2" make it 1 + 2
+  const numbers = stringToAdd.split(",");
+  let sum = 0;
+  
+  for (const number of numbers) {
+    sum += Number(number);
+  }
+  
+  return sum;
+}
