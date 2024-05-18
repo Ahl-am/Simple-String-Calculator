a3a800b26053d6573d4e7b9a14dc6d1abc2d34ee
Ahl-am <ahlam.moh.ibrahim@gmail.com>
2024-05-19 01:40:38 +0300

    ADD: Custom Separator

--- __test__/SimpleStringCalculator.test.ts
+++ __test__/SimpleStringCalculator.test.ts
@@ -7,7 +7,26 @@ describe("Test SimpleStringCalculator", () => {
   it("Should add one number in the string", () => {
     expect(sumFunction("1")).toEqual(1);
   });
-  it("Should add two number in the string", () => {
+  it("Should add two numbers in the string", () => {
     expect(sumFunction("1,2")).toEqual(3);
+  });
+  it("Should add three numbers. The third is after a separator", () => {
+    expect(sumFunction("1,2\n3")).toEqual(6);
+  });
+  it("Should throw an exception if last char is a separator", () => {
+    expect(() => sumFunction("1,2\n3,")).toThrow();
+  });
+  it("Should add a number with custom separator", () => {
+    expect(sumFunction("//;7")).toEqual(7);
+  });
+
+  it("Should add two numbers with custom separator", () => {
+    expect(sumFunction("//;4\n7")).toEqual(11);
+  });
+  it("Should add three numbers with custom separator", () => {
+    expect(sumFunction("//;4\n7;8")).toEqual(19);
+  });
+  it("Should throw an exception if last char is a custom separator", () => {
+    expect(() => sumFunction("//;1,2\n3;")).toThrow();
   });
 });
--- src/SimpleStringCalculator.ts
+++ src/SimpleStringCalculator.ts
@@ -1,11 +1,52 @@
 export const sumFunction = (stringToAdd: string): number => {
-  // if "1,2" make it 1 + 2
-  const numbers = stringToAdd.split(",");
-  let sum = 0;
-  
-  for (const number of numbers) {
-    sum += Number(number);
+  let customSeparator;
+  if (isStringHasCustomSeparator(stringToAdd)) {
+    customSeparator = extractCustomSeparator(stringToAdd);
   }
-  
-  return sum;
-}
+  const updatedString = removeLeadingSlashes(stringToAdd);
+  validateSeparator(updatedString, customSeparator);
+  const numbers = extractNumbers(updatedString, customSeparator);
+  return calculateSum(numbers);
+};
+
+const extractNumbers = (
+  stringToAdd: string,
+  customSeparator?: string
+): number[] => {
+  const regexPattern = getSeparatorRegexExpression(customSeparator);
+  return stringToAdd.split(regexPattern).map(Number);
+};
+
+const validateSeparator = (
+  stringToAdd: string,
+  customSeparator?: string
+): void => {
+  const lastChar = stringToAdd[stringToAdd.length - 1];
+  const regexPattern = getSeparatorRegexExpression(customSeparator);
+  const isLastCharSeparator = regexPattern.test(lastChar);
+  if (isLastCharSeparator) {
+    throw new Error("Invalid argument: Contains separator at the end");
+  }
+};
+
+const extractCustomSeparator = (stringToAdd: string): string | undefined => {
+  const regex = /\/\/([^0-9\n,]*)/;
+  const match = stringToAdd.match(regex);
+  return match && match.length > 0 ? match[1] : undefined;
+};
+
+const isStringHasCustomSeparator = (stringToAdd: string): boolean => {
+  return stringToAdd.startsWith("//");
+};
+
+const removeLeadingSlashes = (str: string): string => {
+  return str.replace(/^\/\//, "");
+};
+
+const calculateSum = (numbers: number[]): number => {
+  return numbers.reduce((sum, number) => sum + number, 0);
+};
+
+const getSeparatorRegexExpression = (customSeparator?: string): RegExp => {
+  return customSeparator ? new RegExp(`,|\\n|${customSeparator}`, "g") : new RegExp(',|\n', "g");
+};
